6e81a299ba23bc497bca70fd5d82f597073626f3
jcplessis <jcplessis@3b747d5a-b732-11dd-8af8-77a15857c330>
2009-09-03 19:40:15 +0000

    New version number
    git-svn-id: https://pyconcordion.googlecode.com/svn/trunk@79 3b747d5a-b732-11dd-8af8-77a15857c330

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='PyConcordion',
-      version='0.3.0',
+      version='0.4.0',
       description='Concordion python port',
       author='Plessis Jean-Christophe',
       author_email='jcplessis@gmail.com',
