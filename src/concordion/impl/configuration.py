bf2558581e4b269f8303f1131e735b219ae39fc5
jcplessis <jcplessis@3b747d5a-b732-11dd-8af8-77a15857c330>
2009-09-06 15:38:38 +0000

    Adding new configurations to handle command line properties
    git-svn-id: https://pyconcordion.googlecode.com/svn/trunk@80 3b747d5a-b732-11dd-8af8-77a15857c330

--- src/concordion/impl/configuration.py
+++ src/concordion/impl/configuration.py
@@ -1,14 +1,13 @@
-
-class FileConfiguration:
-    def __init__(self, filename):
-        self.filename = filename
-        self.properties = None
-        
+class AbstractConfiguration:
+    properties = None
+    
     def _init(self):
-        if not self.properties:
+         if not self.properties:
             self.properties = {}
-            execfile(self.filename, self.properties)
-            del self.properties['__builtins__']
+            self._specific_init()
+    
+    def _specific_init(self):
+        raise Exception('Abstract')
     
     def keys(self):
         self._init()
@@ -17,4 +16,31 @@ class FileConfiguration:
     def get(self, keyname):
         self._init()
         return self.properties[keyname]
+
+class FileConfiguration(AbstractConfiguration):
+    def __init__(self, filename):
+        self.filename = filename
+        
+    def _specific_init(self):
+       execfile(self.filename, self.properties)
+       del self.properties['__builtins__']
     
+class DictionnaryConfiguration(AbstractConfiguration):
+    def __init__(self, properties):
+        self.properties = properties
+        
+    def _specific_init(self):
+        pass
+    
+class HierarchicalConfiguration:
+    def __init__(self, configurations):
+        self.configurations = configurations
+    
+    def get(self, keyname):
+        for configuration in self.configurations:
+            try:
+                return configuration.get(keyname)
+            except Exception, e:
+                pass
+        raise KeyError(keyname)
+        
--- src/concordion/impl/tests/configuration_test.py
+++ src/concordion/impl/tests/configuration_test.py
@@ -1,6 +1,6 @@
-from concordion.impl.configuration import FileConfiguration
-import unittest
-import os
+from concordion.impl.configuration import FileConfiguration, DictionnaryConfiguration, HierarchicalConfiguration
+import unittest, pmock
+import os, sys
 
 class FileConfigurationTest(unittest.TestCase):
     
@@ -34,3 +34,59 @@ class FileConfigurationTest(unittest.TestCase):
         self.file.flush()
         configuration = self._getConfig()
         self.assertEquals(1337, configuration.get("polop"))
+
+class DictionnaryConfigurationTest(unittest.TestCase):
+    
+    def _getConfig(self, properties):
+        return DictionnaryConfiguration(properties)
+    
+    def test_can_take_empty_properties(self):
+        "Dictionnary Configuration - Can be empty"
+        configuration = self._getConfig({})
+        self.assertEquals(0, len(configuration.keys()))
+        
+    def test_can_take_dictionnary(self):
+        "Dictionnary Configuration - Can take a dictionnary with one entry"
+        configuration = self._getConfig({"output_folder":"/tmp/polop"})
+        self.assertEquals(1, len(configuration.keys()))
+        self.assertEquals("/tmp/polop", configuration.get("output_folder"))
+
+class HierarchicalConfigurationTest(unittest.TestCase):
+    
+    def test_cant_take_one_configuration(self):
+        "Hierarchical Configuration - Can take one child configuration"
+        fake_config = pmock.Mock()
+        fake_config.expects(pmock.once()).get(pmock.eq("output_folder")).will(pmock.return_value("polop"))
+        configuration = HierarchicalConfiguration([fake_config])
+        self.assertEquals("polop", configuration.get("output_folder"))
+    
+    def test_cant_take_two_configuration_first_is_default(self):
+        "Hierarchical Configuration - Can take two childs configuration, first child value is returned if exist"
+        fake_config = pmock.Mock()
+        fake_config.expects(pmock.once()).get(pmock.eq("output_folder")).will(pmock.return_value("polop"))
+        other_config = pmock.Mock()
+        configuration = HierarchicalConfiguration([fake_config, other_config])
+        self.assertEquals("polop", configuration.get("output_folder"))
+        
+    def test_cant_take_two_configuration_second_is_called_if_necessary(self):
+        "Hierarchical Configuration - Can take two childs configuration, second child is called if necessary"
+        fake_config = pmock.Mock()
+        fake_config.expects(pmock.once()).get(pmock.eq("output_folder")).will(pmock.raise_exception(Exception("Uknown key...")))
+        other_config = pmock.Mock()
+        other_config.expects(pmock.once()).get(pmock.eq("output_folder")).will(pmock.return_value("polop"))
+        configuration = HierarchicalConfiguration([fake_config, other_config])
+        self.assertEquals("polop", configuration.get("output_folder"))                                                                      
+        
+    def test_raises_an_exception_if_not_found(self):
+        "Hierarchical Configuration - Raises an exception if not found"
+        fake_config = pmock.Mock()
+        fake_config.expects(pmock.once()).get(pmock.eq("output_folder")).will(pmock.raise_exception(Exception("Uknown key...")))
+        other_config = pmock.Mock()
+        other_config.expects(pmock.once()).get(pmock.eq("output_folder")).will(pmock.raise_exception(Exception("Uknown key...")))
+        configuration = HierarchicalConfiguration([fake_config, other_config])
+        try:
+            configuration.get("output_folder")
+            self.fail("Should have thrown exception")
+        except KeyError, e:
+            self.assertEquals("'output_folder'", str(e))
+        
