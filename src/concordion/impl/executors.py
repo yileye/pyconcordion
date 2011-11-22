315261215f75e686b10579c1e660eefca1a9da71
jcplessis <jcplessis@3b747d5a-b732-11dd-8af8-77a15857c330>
2011-11-22 19:38:27 +0000

    Better way to handle printing to stdout (Thanks N. Eybert)
    git-svn-id: https://pyconcordion.googlecode.com/svn/trunk@100 3b747d5a-b732-11dd-8af8-77a15857c330

--- src/concordion/impl/executors.py
+++ src/concordion/impl/executors.py
@@ -1,12 +1,7 @@
 import subprocess
+import sys
 
 class CommandExecutor:
-    def run(self, command, display_output=False):
-        process =  subprocess.Popen(command, shell=True,
-                         stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-        res = process.wait()
-        if res != 0 or display_output: 
-            for line in process.stdout:
-                print line,
-        return res
+	def run(self, command, display_output=False):
+		process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=sys.stdout)
+		return process.wait()
